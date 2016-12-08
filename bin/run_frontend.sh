929167c83a7e7cb694429195bcb8a0016f324887
MJ Berends <mj@jellyvision.com>
2016-12-07 19:41:57 -0600

    Copy node_modules from a separate in-build directory to the volume-linked directory when running the frontend

--- bin/run_frontend.sh
+++ bin/run_frontend.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+# Copy over the node_modules to the volumed directory (they are created on build, but not stored in the git repo)
+cp -r /opt/src/* /opt/trans_passports
+
 npm start
